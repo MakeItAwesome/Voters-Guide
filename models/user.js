9b415ae385d959abc53e6414d0596d68d72fb233
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-02 18:02:13 -0700

    saving progress, about to add uniqueness checker

--- models/user.js
+++ models/user.js
@@ -36,13 +36,29 @@ UserSchema.pre('save', function(next) {
   // before saving
   let user = this;
 
-  // encrypt password
-  bcrypt.hash(user.password, 12, function(err, hash) {
-    if (err) return next(err);
+  // generate code name
+  attempt = 1
 
-    user.password = hash;
-    next();
-  })
+  if (typeof codeName == 'undefined') {
+    let codeName = generateCodeName(user.name,attempt);
+    attempt += 1;
+  } else {
+    while (isUnique(codeName) == false) {
+      let codeName = generateCodeName(user.name,attempt);
+      attempt += 1;
+    }
+  }
+
+  if (isUnique(codeName) == true) {
+    console.log("unique codename generated: " + codeName)
+    // encrypt password
+    bcrypt.hash(user.password, 12, function(err, hash) {
+      if (err) return next(err);
+
+      user.password = hash;
+      next();
+    })
+  }
 });
 
 UserSchema.statics.authenticate = function(email, password, next) {
@@ -66,6 +82,36 @@ UserSchema.statics.authenticate = function(email, password, next) {
       });
     });
 }
+
+function generateCodeName(name, count) {
+  let kabobName = name.split(' ').join('-'); // replace spaces with dashes
+  if (count == 1) {
+    let codeName = kabobName;
+    return codeName;
+  } else {
+    let maxNumber = Math.pow(2,count); // using 2^count to keep it low
+    let randomNumber = getRandomInt(2,maxNumber);
+    let codeName = kabobName + randomNumber;
+    console.log('generated code name');
+    return codeName;
+  }
+}
+
+
+function getRandomInt(min, max) {
+  // Returns a random integer between min (inclusive) and max (inclusive)
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function isUnique(codeName) {
+  if 
+  console.log('codename is unique' + codeName);
+  // look up user by codeName
+    // if unique, return true
+    // else return false
+  return true;
+}
+
 
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
