2c274945a77a6971be7d5837c370512dfcf16766
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-04 17:25:40 -0800

    mvp of update

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,6 @@ router.use(function(req, res, next) {
   res.locals.title = "Voter's Guide";
   // so we can check if user is logged in
   res.locals.user = req.session.user;
-
   next();
 });
 
@@ -20,7 +19,9 @@ router.get('/', function(req, res, next) {
     if (err) {
       console.error(err);
     } else {
-      res.render('index', { props: props });
+      res.render('index', {
+        props: props
+      });
     }
   })
 });
@@ -31,7 +32,9 @@ router.get('/admin', function(req, res, next) {
     if (err) {
       console.error(err);
     } else {
-      res.render('admin', { props: props });
+      res.render('admin', {
+        props: props
+      });
     }
   })
 
@@ -44,11 +47,8 @@ router.get('/admin/props/new', function(req, res, next) {
 
 // POST/CREATE NEW prop
 router.post('/admin/props', function(req, res, next) {
-
-
   let prop = new Proposition(req.body);
-
-  prop.save(function(err, event) {
+  prop.save(function(err, Prop) {
     if (err) {
       console.error(err)
     };
@@ -56,4 +56,40 @@ router.post('/admin/props', function(req, res, next) {
   });
 });
 
+/* GET NEW prop form. */
+router.get('/admin/props/:id/edit', function(req, res, next) {
+  Proposition.findById(req.params.id, function(err, prop) {
+    if (err) {
+      console.error(err)
+    };
+    res.render('props/edit', {
+      prop: prop
+    });
+  });
+});
+
+// PUT/EDIT prop
+router.post('/admin/props/:id', function(req, res, next) {
+  //findByIdAndUpdate
+  //update with request object.body
+  let updatedProp = new Proposition(req.body);
+
+  Proposition.findByIdAndUpdate(
+    req.params.id, {
+      $set: {
+        name: updatedProp.name,
+        summary: updatedProp.summary,
+        pros: updatedProp.pros,
+        cons: updatedProp.cons,
+        readMoreUrl: updatedProp.readMoreUrl,
+        area: updatedProp.area
+      }
+    },
+    function(err, prop) {
+      if (err) {
+        console.error(err)
+      };
+      res.redirect('/admin');
+    });
+});
 module.exports = router;
