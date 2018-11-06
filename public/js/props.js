a4392e575cd1776a43a3b93948333875f179869d
Nicolai Safai <nicolaisafai@gmail.com>
2018-11-05 19:59:04 -0800

    everything so far, stuck at changing votes twice

--- public/js/props.js
+++ public/js/props.js
@@ -1,10 +1,31 @@
-function saveVote(propId) {
+function saveYesVote(propId, user) {
+  console.log("clicked on YES side");
     axios.post('/save-vote', {
         yesVote: propId
     }).then(res => {
-        console.log('Saved Vote');
-        const savedVote = document.getElementById(propId+'-yes-button');
-        savedVote.style.backgroundColor = 'lightgreen';
+        document.getElementById(propId+'-yes').classList.add("prop-voted");
+        document.getElementById(propId+'-yes').innerHTML = 'Undo Vote';
+        document.getElementById(propId+'-yes').setAttribute("onClick", "undoYesVote('{{prop.id}}')");
+
+        document.getElementById(propId+'-no').classList.remove("prop-voted");
+        document.getElementById(propId+'-no').innerHTML = 'Save Vote';
+        document.getElementById(propId+'-no').setAttribute("onClick", "saveNoVote('{{prop.id}}')");
+    }).catch(error => {
+        console.error(error);
+    });
+}
+function saveNoVote(propId, user) {
+  console.log("clicked on NO side");
+    axios.post('/save-vote', {
+        noVote: propId
+    }).then(res => {
+        document.getElementById(propId+'-no').classList.add("prop-voted");
+        document.getElementById(propId+'-no').innerHTML = 'Undo Vote';
+        document.getElementById(propId+'-no').setAttribute("onClick", "undoNoVote('{{prop.id}}')");
+
+        document.getElementById(propId+'-yes').classList.remove("prop-voted");
+        document.getElementById(propId+'-yes').innerHTML = 'Save Vote';
+        document.getElementById(propId+'-yes').setAttribute("onClick", "saveYesVote('{{prop.id}}')");
     }).catch(error => {
         console.error(error);
     });
