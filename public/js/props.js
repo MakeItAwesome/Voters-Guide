8228a02772cee2780717cb0f80e6852554dbd803
Asim Zaidi <asimzaidih@gmail.com>
2018-11-05 20:45:31 -0800

    created undoYesVote undoNoVote functions

--- public/js/props.js
+++ public/js/props.js
@@ -1,31 +1,72 @@
-function saveYesVote(propId, user) {
+function saveYesVote(propId) {
   console.log("clicked on YES side");
     axios.post('/save-vote', {
         yesVote: propId
     }).then(res => {
-        document.getElementById(propId+'-yes').classList.add("prop-voted");
-        document.getElementById(propId+'-yes').innerHTML = 'Undo Vote';
-        document.getElementById(propId+'-yes').setAttribute("onClick", "undoYesVote('{{prop.id}}')");
+        const yesButton = document.getElementById(propId+'-yes');
+        yesButton.classList.add("prop-voted");
+        yesButton.innerHTML = 'Undo Vote';
+        yesButton.setAttribute("onClick", `undoYesVote('${propId}')`);
 
-        document.getElementById(propId+'-no').classList.remove("prop-voted");
-        document.getElementById(propId+'-no').innerHTML = 'Save Vote';
-        document.getElementById(propId+'-no').setAttribute("onClick", "saveNoVote('{{prop.id}}')");
+        const noButton = document.getElementById(propId+'-no');
+        noButton.classList.remove("prop-voted");
+        noButton.innerHTML = 'Save Vote';
+        noButton.setAttribute("onClick", `saveNoVote('${propId}')`);
     }).catch(error => {
         console.error(error);
     });
 }
-function saveNoVote(propId, user) {
+
+function undoYesVote(propId) {
+    console.log("clicked on undo YES side");
+      axios.post('/save-vote', {
+          yesVote: propId
+      }).then(res => {
+          const yesButton = document.getElementById(propId+'-yes');
+          yesButton.innerHTML = 'Save Vote';
+          yesButton.classList.remove("prop-voted");
+          yesButton.setAttribute("onClick", `saveYesVote('${propId}')`);
+  
+          const noButton = document.getElementById(propId+'-no');
+          noButton.classList.remove("prop-voted");
+          noButton.innerHTML = 'Save Vote';
+          noButton.setAttribute("onClick", `saveNoVote('${propId}')`);
+      }).catch(error => {
+          console.error(error);
+      });
+}
+
+function undoNoVote(propId) {
+    console.log("clicked on undo NO side");
+      axios.post('/save-vote', {
+          noVote: propId
+      }).then(res => {
+          const noButton = document.getElementById(propId+'-no');
+          noButton.innerHTML = 'Save Vote';
+          noButton.classList.remove("prop-voted");
+          noButton.setAttribute("onClick", `saveNoVote('${propId}')`);
+          
+          const yesButton = document.getElementById(propId+'-yes');
+          yesButton.classList.remove("prop-voted");
+          yesButton.innerHTML = 'Save Vote';
+          yesButton.setAttribute("onClick", `saveYesVote('${propId}')`);
+      }).catch(error => {
+          console.error(error);
+      });
+  }
+
+function saveNoVote(propId) {
   console.log("clicked on NO side");
     axios.post('/save-vote', {
         noVote: propId
     }).then(res => {
         document.getElementById(propId+'-no').classList.add("prop-voted");
         document.getElementById(propId+'-no').innerHTML = 'Undo Vote';
-        document.getElementById(propId+'-no').setAttribute("onClick", "undoNoVote('{{prop.id}}')");
+        document.getElementById(propId+'-no').setAttribute("onClick", `undoNoVote('${propId}')`);
 
         document.getElementById(propId+'-yes').classList.remove("prop-voted");
         document.getElementById(propId+'-yes').innerHTML = 'Save Vote';
-        document.getElementById(propId+'-yes').setAttribute("onClick", "saveYesVote('{{prop.id}}')");
+        document.getElementById(propId+'-yes').setAttribute("onClick", `saveYesVote('${propId}')`);
     }).catch(error => {
         console.error(error);
     });
